0baa5a0b0cd44114311962b9fba281a5a82678dc
Eládio Alves <eladiolink1@gmail.com>
2024-03-23 23:54:47 -0300

    Ajustes nos ambientes

--- init.lua
+++ init.lua
@@ -15,6 +15,18 @@ for key, mapping in pairs(key_mappings) do
     vim.api.nvim_set_keymap("i", "<F1>", "<Esc>", {noremap = true})
 end
 
+vim.cmd('set signcolumn=yes')   -- Add a column on the left. Useful for linting
+
+if vim.fn.has("nvim") == 1 then -- Verifica se é o Neovim
+    vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+    vim.cmd("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
+end
+
+vim.cmd('highlight Normal guibg=none')
+vim.cmd('highlight NonText guibg=none')
+vim.cmd('highlight Normal ctermbg=none')
+vim.cmd('highlight NonText ctermbg=none')
+
 return {
   -- Configure AstroNvim updates
   updater = {
@@ -35,7 +47,10 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  -- colorscheme = "catppuccin",
+  -- colorscheme = "sonokai",
+  colorscheme =  "dracula",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -99,54 +114,5 @@ return {
     --   },
     -- },
   end,
-   plugins = {
-    {
-      "andweeb/presence.nvim",
-      name = "presence.nvim",
-      config = function()
-        -- The setup config table shows all available config options with their default values:
-require("presence").setup({
-    -- General options
-    auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-    neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
-    main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
-    client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
-    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-    enable_line_number  = false,                      -- Displays the current line number instead of the current project
-    blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-    buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
-    file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-    show_time           = true,                       -- Show the timer
-
-    -- Rich Presence text options
-    editing_text        = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-    file_explorer_text  = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-    git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-    plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-    reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-    workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-    line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
-})
-      end,
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      name = "nvim-treesitter",
-      config = function()
-        require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
-      end,
-    },
-    {
-      "windwp/nvim-ts-autotag",
-      name = "nvim-ts-autotag",
-      config = function()
-        require('nvim-ts-autotag').setup({
-        })
-      end,
-    },{
-      "AstroNvim/astrocommunity",
-    { import = "astrocommunity.colorscheme.catppuccin" }
-    }
-  },
+   plugins = {}
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,4 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
@@ -9,4 +6,166 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+   {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      config = function()
+        require("catppuccin").setup({
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "Macchiato",
+    },
+    transparent_background = true, -- disables setting the background color.
+    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = false, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+    },
+})
+
+      end,
+    },
+   {
+       "sainnhe/sonokai",
+        init = function() 
+         vim.g.sonokai_dim_inactive_windows = 1
+        end,
+   },
+  { 
+    "Mofiqul/dracula.nvim",
+     name="dracula",
+    config=function()
+      require("dracula").setup({
+  colors = {
+    bg = "#282A36",
+    fg = "#F8F8F2",
+    selection = "#44475A",
+    comment = "#6272A4",
+    red = "#FF5555",
+    orange = "#FFB86C",
+    yellow = "#F1FA8C",
+    green = "#50fa7b",
+    purple = "#BD93F9",
+    cyan = "#8BE9FD",
+    pink = "#FF79C6",
+    bright_red = "#FF6E6E",
+    bright_green = "#69FF94",
+    bright_yellow = "#FFFFA5",
+    bright_blue = "#D6ACFF",
+    bright_magenta = "#FF92DF",
+    bright_cyan = "#A4FFFF",
+    bright_white = "#FFFFFF",
+    menu = "#21222C",
+    visual = "#3E4452",
+    gutter_fg = "#4B5263",
+    nontext = "#3B4048",
+    white = "#ABB2BF",
+    black = "#191A21",
+  },
+  show_end_of_buffer = true, -- default false
+  transparent_bg = true, -- default false
+  lualine_bg_color = "#44475a", -- default nil
+  italic_comment = true, -- default false
+  overrides = {},
+      })
+    end,
+  },
+
+  -- Configure LazyVim to load dracula
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+  { import = "astrocommunity.pack.typescript" },
+  {
+    "sigmasd/deno-nvim",
+    -- HACK: This disables tsserver if denols is attached.
+    -- A solution that only enables the required lsp should replace it.
+    opts = function(_, opts)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local curr_client = vim.lsp.get_client_by_id(args.data.client_id)
+          -- if deno attached, stop all typescript servers
+          if curr_client.name == "denols" then
+            vim.lsp.for_each_buffer_client(bufnr, function(client, client_id)
+              if client.name == "tsserver" then vim.lsp.stop_client(client_id, true) end
+            end)
+            -- if tsserver attached, stop it if there is a denols server attached
+            elseif curr_client.name == "tsserver" then
+            for _, client in ipairs(vim.lsp.get_active_clients { bufnr = bufnr }) do
+              if client.name == "denols" then
+                vim.lsp.stop_client(curr_client.id, true)
+                break
+              end
+            end
+          end
+        end,
+      })
+      opts.server = require("astronvim.utils.lsp").config "denols"
+      opts.server.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+          autotag = { 
+            enable = true,
+            enable_rename = true,
+            enable_close = true,
+            enable_close_on_slash = true,
+          },
+        })
+    end
+  },
+  {
+    'davidhalter/jedi-vim',
+    name="jedi-vim",
+    config=function()
+      require("jedi-vim").setup({})
+    end,
+  },
 }
